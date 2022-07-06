9e6cfa747ade5dfbd8586ded1b72a3096f06f51d
Niels <niels.koster@portavita.nl>
2022-07-06 23:28:02 +0200

    Show amount of users in channel

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import './Header.css'
 import {useChatState} from '../context/ChatContext'
 import useLocalStorage from '../hooks/LocalStorage'
+import {useUsersState} from '../context/UsersContext'
 
 export default function Header({logout}) {
   const {chatState} = useChatState()
   const {storedValue} = useLocalStorage('userInfo')
+  const {usersState} = useUsersState()
+  console.log('usersState', usersState)
+
   function handleOnClick() {
     logout()
   }
@@ -12,6 +16,7 @@ export default function Header({logout}) {
     <div className='Header'>
       <div>{storedValue.username}@{storedValue.channel}</div>
       <div>{chatState.chatLines.length} line{chatState.chatLines.length === 1 ? '' : 's'}</div>
+      <div>{usersState.length} user{usersState.length === 1 ? '' : 's'}</div>
       <button onClick={handleOnClick}>logout</button>
     </div>
   )
