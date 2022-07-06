69116717aa8990784d13b99fbff1ecaa663b358f
Niels <niels.koster@portavita.nl>
2022-07-07 00:36:16 +0200

    Make sure websockets work in dev and non-dev

--- src/context/WebsocketContext.js
+++ src/context/WebsocketContext.js
@@ -14,7 +14,17 @@ export function useWebsocket() {
 
 export default function WebsocketProvider({children}) {
   const {storedValue} = useLocalStorage('userInfo')
-  const socket = io('ws://localhost:3011', {
+  const url = window.location.href
+  const host = window.location.host
+  const port = window.location.port
+  const isHttps = url.includes('https:')
+  let socketUrl
+  if (port === '3000') {
+    socketUrl = 'ws://localhost:3011'
+  } else {
+    socketUrl = `ws${isHttps ? 's' : ''}://${host}`
+  }
+  const socket = io(socketUrl, {
     auth: [storedValue?.channel],
   })
   const websocket = useRef(socket)
