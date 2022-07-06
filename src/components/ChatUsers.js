143d3e34fe05d2b9ccc515976cc7cc9998429f05
Niels <niels.koster@portavita.nl>
2022-07-07 01:40:50 +0200

    Clear out any duplicates in the user list

--- src/components/ChatUsers.js
+++ src/components/ChatUsers.js
@@ -1,4 +1,4 @@
-import {useEffect} from 'react'
+import {useEffect, useRef} from 'react'
 import './ChatUsers.css'
 import {useWebsocket} from '../context/WebsocketContext'
 import useLocalStorage from '../hooks/LocalStorage'
@@ -10,6 +10,7 @@ export default function ChatUsers() {
   const {storedValue} = useLocalStorage('userInfo')
   const {usersState, setUsersState} = useUsersState()
   const {websocket} = useWebsocket()
+  const unique = useRef([])
 
   useEffect(() => {
     websocket.on('newlist', l => {
@@ -18,9 +19,23 @@ export default function ChatUsers() {
     return () => websocket.off('newlist')
   }, [])
 
+  useEffect(() => {
+    if (usersState.filter) {
+      unique.current = usersState.filter && usersState.filter(chan => chan.name === storedValue.channel)
+        .filter(chan => {
+          const isDup = unique.current.includes(chan.user)
+          if (!isDup) {
+            unique.current.push(chan)
+            return true
+          }
+          return false
+        })
+    }
+  }, [usersState])
+
   return (
     <div className='ChatUsers'>
-      {usersState.filter && usersState.filter(chan => chan.name === storedValue.channel)
+      {unique.current.filter && unique.current.filter(chan => chan.name === storedValue.channel)
         .map((chan, index) => <ChatUser key={index} myKey={index} user={chan.user} />)}
     </div>
   )
