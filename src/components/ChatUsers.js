17472986c172333ceaf50e8249324fd086e8a077
Niels <niels.koster@portavita.nl>
2022-07-06 17:52:12 +0200

    Use only the newlist event to get a new user list

--- src/components/ChatUsers.js
+++ src/components/ChatUsers.js
@@ -1,29 +1,33 @@
-import {useEffect, useState} from 'react'
+import {useEffect} from 'react'
 import './ChatUsers.css'
 import {useWebsocket} from '../context/WebsocketContext'
+import useLocalStorage from '../hooks/LocalStorage'
+import {useUsersState} from '../context/UsersContext'
 
-async function loadUserList() {
-  const result = await fetch('http://localhost:3011/userlist')
-    .then(res => res.json())
-    .catch(err => console.log(err.message))
-  console.log(result)
-}
+// async function loadUserList() {
+//   const result = await fetch('http://localhost:3011/userlist')
+//     .then(res => res.json())
+//     .catch(err => console.log(err.message))
+//   console.log('loadUserList', result)
+// }
 
 export default function ChatUsers() {
 
-  const [list, setList] = useState()
+  const {storedValue} = useLocalStorage('userInfo')
+  const {usersState, setUsersState} = useUsersState()
   const {websocket} = useWebsocket()
 
   useEffect(() => {
-    loadUserList()
     websocket.on('newlist', l => {
-      setList(l)
+      setUsersState(l)
     })
+    return () => websocket.off('newlist')
   }, [])
+  console.log('usersState', usersState)
 
   return (
     <div className='ChatUsers'>
-      {list && list.map((chan, index) => <div key={index}>{chan.user}</div>)}
+      {usersState.filter && usersState.filter(chan => chan.name === storedValue.channel).map((chan, index) => <div key={index}>{chan.user}</div>)}
     </div>
   )
 }
