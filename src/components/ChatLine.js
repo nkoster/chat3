71c12fb02dbeab4bbb3d2cd3127a10cb2928ba08
Niels <niels.koster@portavita.nl>
2022-07-11 01:23:06 +0200

    Server lines have different background

--- src/components/ChatLine.js
+++ src/components/ChatLine.js
@@ -2,8 +2,8 @@ import './ChatLine.css'
 
 function ChatLine({mykey, line}) {
   return (
-    <div className='ChatLine'>
-      <div className='ChatLineUser'>{line.user}:</div>
+    <div className={`ChatLine ${line.user === 'server' && 'server'}`}>
+      <div className='ChatLineUser'>{line.user} » </div>
       <div className='ChatLineData' key={mykey}>{line.data}</div>
       <div className='ChatLineTime'>{line.time}</div>
     </div>
